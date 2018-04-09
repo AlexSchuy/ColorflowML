a1dfaf4cae921e633ba498e579bf2a287357d064
Alex Schuy <alexjschuy@gmail.com>
2018-04-09 11:49:05 -0700

    Fixes bugs, adds pipenv package control.

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import os
 
 import constants
@@ -7,19 +7,25 @@ def get_pixels(octet=False, recalculate=False):
   if octet:
     textfile = constants.octet_text
     npyfile = constants.octet_npy
+    print("[data] Getting octet pixel data ...")
   else:
     textfile = constants.singlet_text
     npyfile = constants.singlet_npy
+    print("[data] Getting singlet pixel data ...")
   # Get the appropriate numpy array, either from a saved .npy file or
   # from the original .txt file.
   if recalculate or not os.path.exists(npyfile):
-    with open(textfile) as textfile_handle:
-      pixels = np.loadtxt(textfile_handle, usecols=range(9, 634))
+    if not os.path.exists(npyfile):
+      print("[data] {} not found, attempting to recreate from {} ...".format(npyfile, textfile))
+    pixels = np.loadtxt(textfile, usecols=range(9, 634))
     # Remove any row with a nan value.
+    if (np.isnan(pixels).any()):
+      print("[data] Warning: non-numeric values encountered, removing affected samples.")
     pixels = pixels[~np.isnan(pixels).any(axis=1)]
-    with open(npyfile) as npyfile_handle:
+    with open(npyfile, 'wb+') as npyfile_handle:
       np.save(npyfile_handle, pixels)
   else:
-    with open(npyfile) as npyfile_handle:
-      pixels = np.load(npyfile_handle)
+    print("[data] Loading from {} ...".format(npyfile))
+    pixels = np.load(npyfile)
+  print("[data] {} pixels shape: {}".format("octet" if octet else "singlet", pixels.shape))
   return pixels
--- train.py
+++ train.py
@@ -1,19 +1,19 @@
-from __future__ import print_function
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout, Conv2D, MaxPooling2D
-from keras.layers import BatchNormalization, Dense
-from keras.regularizers import l2
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout, Conv2D, MaxPooling2D
+from tensorflow.python.keras.layers import BatchNormalization, Dense, Flatten
+from tensorflow.python.keras.regularizers import l2
 import os
 
 import data
+import constants
 
 batch_size = 96
 epochs = 100
-save_dir = os.path.join(data_dir, 'saved_models')
+save_dir = os.path.join(constants.data_dir, 'saved_models')
 model_name = 'keras_colorflow_trained_model.h5'
 
 # Load the pixel data, split it into train/test and standardize it.
@@ -21,29 +21,32 @@ X_octet = data.get_pixels(octet=True)
 X_singlet = data.get_pixels(octet=False)
 y_octet = np.zeros(X_octet.shape[0])
 y_singlet = np.ones(X_singlet.shape[0])
-X = np.concat(X_octet, X_singlet)
-y = np.concat(y_octet, y_singlet)
+X = np.concatenate([X_octet, X_singlet])
+y = np.concatenate([y_octet, y_singlet])
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+print("[train] Standardizing data ...")
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train = np.reshape(scaler.fit_transform(X_train), (X_train.shape[0], 25, 25, 1))
+X_test = np.reshape(scaler.transform(X_test), (X_test.shape[0], 25, 25, 1))
+print("[train] training data shape: {}".format(X_train.shape))
+print("[train] testing data shape: {}".format(X_test.shape))
 
 model = Sequential()
 
 # Convolution 1
-model.add(Dropout(0.5))
+model.add(Dropout(0.5, input_shape=(25, 25, 1)))
 model.add(Conv2D(32, (11, 11), padding='same', activation='relu', kernel_regularizer=l2()))
-model.add(MaxPool((2, 2)))
+model.add(MaxPooling2D((2, 2)))
 
 # Convolution 2
 model.add(Dropout(0.5))
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_regularizer=l2()))
-model.add(MaxPool((3, 3)))
+model.add(MaxPooling2D((3, 3)))
 
 # Convolution 3
 model.add(Dropout(0.5))
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_regularizer=l2()))
-model.add(MaxPool((3, 3)))
+model.add(MaxPooling2D((3, 3)))
 
 model.add(BatchNormalization())
 
@@ -53,11 +56,12 @@ model.add(Dense(64, activation='relu', kernel_regularizer=l2()))
 
 # Fully-Connected 2
 model.add(Dropout(0.1))
+model.add(Flatten())
 model.add(Dense(1, activation='relu', kernel_regularizer=l2()))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(x_train, y_train), shuffle=True)
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(X_test, y_test), shuffle=True)
 
 if not os.path.isdir(save_dir):
   os.makedirs(save_dir)
@@ -65,6 +69,6 @@ model_path = os.path.join(save_dir, model_name)
 model.save(model_path)
 print('saved trained model at %s ' % model_path)
 
-scores = model_evaluate(x_test, y_test, verbose=2)
+scores = model_evaluate(X_test, y_test, verbose=2)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
